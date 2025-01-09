ad0954dd75e7cc08fe1ba37dc938117c772c64b0
tampubolon <tampubolonmartinus@gmail.com>
2025-01-09 07:41:12 +0000

    submit task-b2, distribute the compute

---
+++ task-b/task-b2/main.py
@@ -0,0 +1,21 @@
+import time
+from utils.file_utils import ensure_directory_exists
+from parallel_column_processor import ParallelColumnProcessor
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    input_file = "dummy-data.xlsx"
+    parallel_processor = ParallelColumnProcessor(input_file)
+    column_sums = parallel_processor.process()
+    
+
+    print("===========================================================")
+    print("Column Sums:", column_sums)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    print("-----------------------------------------------------------")
+    print(f"Execution time: {execution_time:.4f} seconds")
+    print("===========================================================")
