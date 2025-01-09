ca7f79d3fc14bd02d13e6da8fff89299397a91ce
tampubolon <tampubolonmartinus@gmail.com>
2025-01-09 10:03:44 +0000

    Optimize parallel_column_processor

--- task-b/task-b2/parallel_column_processor.py
+++ task-b/task-b2/parallel_column_processor.py
@@ -10,19 +10,10 @@ class ParallelColumnProcessor:
         self.timestamp = self.column_sum_processor.timestamp
 
     def process_single_column(self, column_name: str, df: pd.DataFrame) -> Dict[str, str]:
-        """
-        Processes a single column using a new instance of ColumnSumProcessor.
-        """
-        temp_file = f"columns-sum/temp_{column_name}-{self.timestamp}.xlsx"
         single_column_df = df[[column_name]]
-        
-        # Save the single column to an Excel file
-        self.column_sum_processor.save_to_excel(single_column_df, temp_file)
-        
-        # Read the file back and process
         numeric_df = self.column_sum_processor.filter_numeric_columns(single_column_df)
         column_sums = self.column_sum_processor.calculate_column_sums(numeric_df)
-        return {column_name: column_sums[column_name]}
+        return {column_name: column_sums[column_name]}    
 
     def process_columns_in_parallel(self) -> Dict[str, str]:
         """
